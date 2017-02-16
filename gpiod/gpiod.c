32d79d52fb06ae0f13f01cd836679afeb6e3661c
Shane Gehring <shane.gehring@gmail.com>
2017-02-16 01:37:41 +0000

    Attempts to map GPIOs to proto plus buttons (wip)

--- gpiod/gpiod.c
+++ gpiod/gpiod.c
@@ -1,36 +1,3 @@
-/*
- * isr.c:
- *	Wait for Interrupt test program - ISR method
- *
- *	How to test:
- *	  Use the SoC's pull-up and pull down resistors that are avalable
- *	on input pins. So compile & run this program (via sudo), then
- *	in another terminal:
- *		gpio mode 0 up
- *		gpio mode 0 down
- *	at which point it should trigger an interrupt. Toggle the pin
- *	up/down to generate more interrupts to test.
- *
- * Copyright (c) 2013 Gordon Henderson.
- ***********************************************************************
- * This file is part of wiringPi:
- *	https://projects.drogon.net/raspberry-pi/wiringpi/
- *
- *    wiringPi is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Lesser General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    wiringPi is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public License
- *    along with wiringPi.  If not, see <http://www.gnu.org/licenses/>.
- ***********************************************************************
- */
-
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
@@ -40,28 +7,19 @@
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 
-// globalCounter:
-//	Global variable to count interrupts
-//	Should be declared volatile to make sure the compiler doesn't cache it.
+/* DACP Daemon Port */
+#define DACPD_PORT 3391
 
-static volatile int globalCounter [8] ;
+/* GPIO Pin Mappings */
+#define GPIO_VOLUME_MUTE  18
+#define GPIO_VOLUME_UP    23
+#define GPIO_VOLUME_DOWN  24
+#define GPIO_PLAY_NEXT    25
+#define GPIO_PLAY_PAUSE   8
+#define GPIO_PLAY_PREV    7
 
-
-/*
- * myInterrupt:
- *********************************************************************************
- */
-
-void myInterrupt0 (void) { ++globalCounter [0] ; }
-void myInterrupt1 (void) { ++globalCounter [1] ; }
-void myInterrupt2 (void) { ++globalCounter [2] ; }
-void myInterrupt3 (void) { ++globalCounter [3] ; }
-void myInterrupt4 (void) { ++globalCounter [4] ; }
-void myInterrupt5 (void) { ++globalCounter [5] ; }
-void myInterrupt6 (void) { ++globalCounter [6] ; }
-void myInterrupt7 (void) { ++globalCounter [7] ; }
-
-void do_cmd(const char* cmd) {
+/* Sends message to DACPD */
+void doCmd(const char* cmd) {
 
   static int sockfd;
   if (sockfd == 0)
@@ -72,62 +30,38 @@ void do_cmd(const char* cmd) {
   struct sockaddr_in si;
   memset(&si, 0, sizeof(si));
   si.sin_family = AF_INET;
-  si.sin_port = htons(3391);
+  si.sin_port = htons(DACPD_PORT);
   inet_aton("127.0.0.1", &si.sin_addr);
 
-  printf("CMD %s\n", cmd);
+  printf("CMD: %s\n", cmd); 
+  fflush(stdout);
   sendto(sockfd, cmd, strlen(cmd)+1, 0, (struct sockaddr *)&si, sizeof(si));
 
 }
 
+/* Handlers */
+void doVolumeMute (void) { doCmd("mutetoggle"); }
+void doVolumeUp   (void) { doCmd("volumeup");   }
+void doVolumeDown (void) { doCmd("volumedown"); }
+void doPlayNext   (void) { doCmd("nextitem");   }
+void doPlayPrev   (void) { doCmd("previtem");   }
+void doPlayPause  (void) { doCmd("playpause");  }
 
-/*
- *********************************************************************************
- * main
- *********************************************************************************
- */
-
+/* Main */
 int main (void)
 {
-  int gotOne, pin ;
-  int myCounter [8] ;
+  wiringPiSetupGpio();
 
-  for (pin = 0 ; pin < 8 ; ++pin) 
-    globalCounter [pin] = myCounter [pin] = 0 ;
+  wiringPiISR(GPIO_VOLUME_MUTE, INT_EDGE_FALLING, &doVolumeMute);
+  wiringPiISR(GPIO_VOLUME_UP,   INT_EDGE_FALLING, &doVolumeUp);
+  wiringPiISR(GPIO_VOLUME_DOWN, INT_EDGE_FALLING, &doVolumeDown);
+  wiringPiISR(GPIO_PLAY_NEXT,   INT_EDGE_FALLING, &doPlayNext);
+  wiringPiISR(GPIO_PLAY_PREV,   INT_EDGE_FALLING, &doPlayPrev);
+  wiringPiISR(GPIO_PLAY_PAUSE,  INT_EDGE_FALLING, &doPlayPause);
 
-  wiringPiSetup () ;
+  printf("Monitoring GPIO activity...\n");
+  fflush(stdout);
+  while(1);
 
-  wiringPiISR (0, INT_EDGE_FALLING, &myInterrupt0) ;
-  wiringPiISR (1, INT_EDGE_FALLING, &myInterrupt1) ;
-  wiringPiISR (2, INT_EDGE_FALLING, &myInterrupt2) ;
-  wiringPiISR (3, INT_EDGE_FALLING, &myInterrupt3) ;
-  wiringPiISR (4, INT_EDGE_FALLING, &myInterrupt4) ;
-  wiringPiISR (5, INT_EDGE_FALLING, &myInterrupt5) ;
-  wiringPiISR (6, INT_EDGE_FALLING, &myInterrupt6) ;
-  wiringPiISR (7, INT_EDGE_FALLING, &myInterrupt7) ;
-
-  for (;;)
-  {
-    gotOne = 0 ;
-    printf ("Waiting ... ") ; fflush (stdout) ;
-
-    for (;;)
-    {
-      for (pin = 0 ; pin < 8 ; ++pin)
-      {
-	if (globalCounter [pin] != myCounter [pin])
-	{
-	  printf (" Int on pin %d: Counter: %5d\n", pin, globalCounter [pin]) ;
-	  myCounter [pin] = globalCounter [pin] ;
-	  ++gotOne ;
-          do_cmd(myCounter[pin] & 1 ? "volumeup" : "nextitem");
-	}
-      }
-      if (gotOne != 0)
-	break ;
-    }
-  }
-
-  return 0 ;
+  return 0;
 }
-
